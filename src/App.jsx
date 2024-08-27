da8cb76e9f094b22a6cdf3d40d0fd86d6f5523ec
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-27 17:33:44 +0900

    +-のボタン追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment} from "./redux/counterSlice";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <h1>count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>＋</button>
+      <button onClick={() => dispatch(decrement())}>ー</button>
     </div>
   );
 }
