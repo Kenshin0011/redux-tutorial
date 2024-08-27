5d6ca2a2c9405aef9aa19eed64bdee0c4b884a19
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-27 17:36:59 +0900

    incrementByAmount

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
-import {decrement, increment} from "./redux/counterSlice";
+import {decrement, increment, incrementByAmount} from "./redux/counterSlice";
+import {useState} from "react";
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
+
+  const [incrementAmount, setIncrementAmount] = useState("2");
+
   return (
     <div className="App">
       <h1>count: {count}</h1>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount} />
       <button onClick={() => dispatch(increment())}>＋</button>
       <button onClick={() => dispatch(decrement())}>ー</button>
+      <button onClick={() => dispatch(incrementByAmount(Number(incrementAmount)))}>追加</button>
     </div>
   );
 }
