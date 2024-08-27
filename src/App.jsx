a49a83298f533445177fb43e664111ea9d8e0b6a
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-08-27 17:30:09 +0900

    valueの反映

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
+import {useSelector} from "react-redux";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
   return (
     <div className="App">
-
+      <h1>count: {count}</h1>
     </div>
   );
 }
